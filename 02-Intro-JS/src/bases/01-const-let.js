20618cc436fafe444f4d7cc51cde9f22609e839a
Natalia Solorzano <00120319@uca.edu.sv>
2022-12-19 14:53:20 -0600

    02-Intro-JS

---
+++ 02-Intro-JS/src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+// Variables y Constantes
+
+const nombre = 'Natalia';
+const apellido = 'Solorzano';
+
+let valorDado = 5;
+valorDado = 3;
+
+console.log(nombre, apellido, valorDado)
+
+if ( true ) {
+    const nombre = 'Peter';
+
+    let valorDado = 6;
+
+    console.log(valorDado);
+    console.log(nombre);
+}
+
+console.log( valorDado );
